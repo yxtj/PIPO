daa19de13499c393a63d3c103e039f4a08643f48
Tian Zhou <yanxiangtianji@gmail.com>
2023-08-01 16:09:19 -0400

    chore: setting name

--- setting.py
+++ setting.py
@@ -3,15 +3,14 @@ import os
 __all__ = ['USE_HE', 'PROTOCOL']
 
 # whether to use HE in the offline phase
-USE_HE = os.environ.get('FASENET_USE_HE', '0').lower() in ['1', 'true', 't', 'yes', 'y']
+USE_HE = os.environ.get('SCALENET_USE_HE', '0').lower() in ['1', 'true', 't', 'yes', 'y']
 
 print("USE_HE:", USE_HE)
 
 # which protocol to use
-PROTOCOL = os.environ.get('FASENET_PROTOCOL', 'scale')
+PROTOCOL = os.environ.get('SCALENET_PROTOCOL', 'scale')
 # protocols: scale, dual, shuffle, pad
-# scale and dual are mutually exclusive
-# dual is scale + oblivious transfer (OT)
+# dual is scale + dual
 # shuffle is scale + shuffle
 # pad is scale + shuffle + pad
 print("PROTOCOL:", PROTOCOL)
