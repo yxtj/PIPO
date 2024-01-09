8a4f7561888dd47c33d2cde27c915e5469b24b74
yxtj <yanxiangtianji@gmail.com>
2024-01-09 04:33:26 -0500

    extract

--- security/extract.py
+++ security/extract.py
@@ -4,6 +4,14 @@ import torch
 import torch.nn as nn
 
 
+def gen_preimage_model(conv: nn.Conv2d):
+    r = nn.ConvTranspose2d(conv.in_channels, conv.out_channels, conv.kernel_size,
+                           padding=conv.padding, bias=conv.bias is not None)
+    w = torch.flip(conv.weight.data, [2,3])/conv.weight.data.sum()
+    r.weight.data = w
+    return r
+
+
 @torch.no_grad()
 def gradient(model, x, step):
     # assert x.shape == step.shape
@@ -13,7 +21,8 @@ def gradient(model, x, step):
     for i in range(c):
         for j in range(h):
             for k in range(w):
-                xp = x + step[i,j,k]
+                xp = x.clone()
+                xp[...,i,j,k] += step[i,j,k]
                 rp = model(xp)
                 res[i,j,k] = (rp-r)/step[i,j,k]
     return res
@@ -23,33 +32,85 @@ def check_layer(model, x, eps):
     pass
 
 
-def binary_prob(model, f, l, step):
-    pass
+def binary_probe_cp_idx(model, start, step, zero_thr, f, l, df, dl):
+    if torch.abs(df - dl) <= zero_thr:
+        return []
+    if f >= l:
+        return [f] if torch.abs(df - dl) > zero_thr else []
+    m = (f + l)//2
+    mid = start + step*m
+    dm = model(mid+step)-model(mid)
+    # left
+    if torch.abs(df - dm) > zero_thr: # df != dm:
+        res1 = binary_probe_cp_idx(model, start, step, zero_thr, f, m, df, dm)
+    else:
+        res1 = []
+    # right
+    if torch.abs(dm - dl) > zero_thr: # dm != dl:
+        res2 = binary_probe_cp_idx(model, start, step, zero_thr, m, l, dm, dl)
+    else:
+        res2 = []
+    return res1 + res2
+    
 
 @torch.no_grad()
-def probe(model, start, end, eps):
+def probe_cp(model, start, end, eps, zero_thr=1e-8):
     dist = torch.dist(start, end)
     n = int(dist//eps)
     step = (end - start) / n
-    res = []
-    i = 0
-    ds = model(start+eps)-model(start)
-    de = model(end+eps)-model(end)
-    while i<n:
-        if torch.abs(ds - de)<1e-8:
-            break
-        mid = start + step*(n//2)
+    
+    df = model(start+step)-model(start)
+    dl = model(end+step)-model(end)
+    idx = binary_probe_cp_idx(model, start, step, zero_thr*eps, 0, n, df, dl)
+    cps = [start+i*step for i in idx]
+    return cps
 
 
-
-    return res
-
-
-def extract(model, eps=1e-6):
-    pass
+def extract(model, start, end, eps=1e-6):
+    # find critical points
+    cps = probe_cp(model, start, end, eps)
+    
+    # identify layers
+    
+    # extract weights
 
 
 # %% test
+
+def __test_binary_probe__():
+    m1 = nn.Sequential(nn.Conv(1,1,3), nn.ReLU())
+    eps = 1e-4
+    
+    start = torch.rand(1,3,3)
+    end = -start
+    rs, re = m1(start), m1(end)
+    print(rs, re)
+    assert rs * re == 0.0 and rs + re > 0.0
+    
+    n = int(torch.dist(start, end)/eps)
+    step = (end-start)/n
+    
+    a,b = start,end
+    while torch.dist(a,b)>eps:
+        ga=m1(a+step)-m1(a)
+        gb=m1(b+step)-m1(b)
+        if torch.abs(ga-gb)/eps<1e-6:
+            print('found', ga/eps, gb/eps)
+            break
+        mid = (a+b)/2
+        gm=m1(mid+step)-m1(mid)
+        t = torch.tensor([torch.dist(start, a)//eps, torch.dist(start, mid)//eps, torch.dist(start, b)//eps, ga, gm, gb])
+        print(t.tolist())
+        if torch.abs(ga-gm)/eps<1e-6:
+            a = mid+step
+            print('right')
+        else:
+            b = mid-step
+            print('left')
+    
+    print(gradient(m1, a, step))
+    print(gradient(m1, b, step))
+
 
 def __test__():
     m0 = nn.Conv2d(1,1,3)
